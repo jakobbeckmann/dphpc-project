6a0f77bfbe8ba739436c41af044d2f64d5522d7b
Matthäus Heer <mathee@physik.uzh.ch>
2017-10-17 23:14:06 +0200

    Minor changes.

--- chan_visualization/Chan2DVisualizer.py
+++ chan_visualization/Chan2DVisualizer.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-import matplotlib as mpl
+# import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 
@@ -10,8 +10,20 @@ class Chan2DVisualizer:
         self.all_points = self.__class__.load_csv_file(all_points_file)
         self.hull_points = self.__class__.load_csv_file(hull_points_file)
 
-    def main(self):
-        self.plot_points()
+    def plot_points(self):
+        fig, ax = plt.subplots()
+        fig.set_size_inches(7, 7)
+
+        ax.scatter(self.all_points[0], self.all_points[1], s=10, c='red', alpha=0.7)
+        ax.scatter(self.hull_points[0], self.hull_points[1], s=20, c='blue')
+
+        for axis in [ax.get_xaxis(), ax.get_yaxis()]:
+            axis.set_ticks([])
+
+        plt.show()
+
+    def start_animation(self):
+        pass
 
     @staticmethod
     def load_csv_file(chan_data_file):
@@ -20,22 +32,15 @@ class Chan2DVisualizer:
         y = [item[1] for item in data]
         return np.array([x, y])
 
-    def plot_points(self):
-        fig, ax = plt.subplots(figsize=(7, 7))
-        ax.scatter(self.all_points[0], self.all_points[1], s=10, c='red', alpha=0.7)
-        ax.scatter(self.hull_points[0], self.hull_points[1], s=20, c='blue')
 
-        ax.get_xaxis().set_ticks([])
-        ax.get_yaxis().set_ticks([])
-        plt.show()
-
-    def start_animation(self):
-        pass
-
-
-if __name__ == '__main__':
-    all_points_file  = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'cmake-build-debug', 'all_points.dat')
-    hull_points_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'cmake-build-debug', 'hull_points.dat')
+def main():
+    data_base_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'cmake-build-debug')
+    all_points_file = os.path.join(data_base_path, 'all_points.dat')
+    hull_points_file = os.path.join(data_base_path, 'hull_points.dat')
 
     chan_visualizer = Chan2DVisualizer(all_points_file, hull_points_file)
-    chan_visualizer.main()
+    chan_visualizer.plot_points()
+
+if __name__ == '__main__':
+
+    main()
