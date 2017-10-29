8d10efc73be10562508edc1cc0ee62c17906a27b
Matthäus Heer <mathee@physik.uzh.ch>
2017-10-30 00:52:29 +0100

    Added some documentation

--- Visualization/ChanAnimation.py
+++ Visualization/ChanAnimation.py
@@ -7,19 +7,24 @@ from matplotlib.pyplot import cm
 import numpy as np
 
 
-class GrahamAnimation:
+class ChanAnimation:
     def __init__(self, data_dict, label_points):
         """
             - state represents the 3 indices of base_idx, last_idx, last_idx - 1, used to draw the arrows
             - hull_points is the stack of points that lie on the hull (at a given moment)
         """
 
+        # ----- raw data
         self.all_points         = data_dict['sub_sorted']
         self.final_hull         = data_dict['total_hull']
         self.final_sub_hulls    = data_dict['hull_points']
         self.graham_run         = data_dict['graham_runs']
         self.n_graham_subs      = data_dict['n_graham_subs']
+        self.merge_history      = data_dict['merge_hulls']
 
+        self.ax                 = None
+
+        # ----- graham subset animation part
         self.curr_sub_hull      = {idx: {'x': [self.all_points[idx]['x'][add_idx] for add_idx in [-1, 0, 1]],
                                          'y': [self.all_points[idx]['y'][add_idx] for add_idx in [-1, 0, 1]]}
                                    for idx in range(self.n_graham_subs)}
@@ -29,17 +34,14 @@ class GrahamAnimation:
         self.n_steps_graham     = {idx: len(self.graham_run[idx]) for idx in range(self.n_graham_subs)}
         self.two_lines          = {idx: None for idx in range(self.n_graham_subs)}
         self.hull_lines         = {idx: None for idx in range(self.n_graham_subs)}
-
         self.two_lines_color    = {'clockwise': 'black', 'anticlockwise': 'green'}
         self.step               = 0
-        self.ax                 = None
-
-        self.colors             = cm.rainbow(np.linspace(0, 1, self.n_graham_subs))
+        self.graham_colors      = cm.rainbow(np.linspace(0, 1, self.n_graham_subs))
         self.label_points       = label_points
 
-        self.merge_history      = data_dict['merge_hulls']
+        # ----- merge subsets animation part
         self.n_steps_merge      = len(self.merge_history['x'])
-        self.merge_color        = 'cyan'
+        self.merge_color        = 'black'
         self.merge_lines        = None
 
         # self.idx_stack = [len(self.all_points['x']) - 1, 0, 1]  # unused
@@ -55,6 +57,8 @@ class GrahamAnimation:
             del self.idx_stack[-2]
 
     def update_graham_state(self, step, idx):
+        """Updates the state of the [idx] graham subanimation to step [step]."""
+
         self.state[idx] = self.graham_run[idx][step][:6]
 
         for i, item in enumerate(self.state):
@@ -80,6 +84,7 @@ class GrahamAnimation:
             self.curr_orientation[idx] = 'anticlockwise'
 
     def get_two_lines_pos(self, idx):
+        """Returns a (x1, x2, x3, y1, y2, y3) tuple for the three points of interest for the [idx] graham sub hull."""
         # x = [self.all_points['x'][idx] for idx in self.idx_stack[-3:]]
         # y = [self.all_points['y'][idx] for idx in self.idx_stack[-3:]]
         x = self.state[idx][0: 3]
@@ -87,6 +92,7 @@ class GrahamAnimation:
         return x, y
 
     def get_hull_lines_pos(self, idx):
+        """Returns (x..., y...) tuple for the current points of the graham [idx] sub hull."""
         # x = [self.all_points['x'][idx] for idx in self.idx_stack]
         # y = [self.all_points['y'][idx] for idx in self.idx_stack]
         x = self.curr_sub_hull[idx]['x']
@@ -94,6 +100,7 @@ class GrahamAnimation:
         return x, y
 
     def get_merge_lines_pos(self, step):
+        """Returns (x..., y...) tuple for the current points of the merged total hull."""
 
         x = self.merge_history['x'][:(step - max(self.n_steps_graham.values()) - 1)]
         y = self.merge_history['y'][:(step - max(self.n_steps_graham.values()) - 1)]
@@ -105,11 +112,12 @@ class GrahamAnimation:
         return x, y
 
     def plot_all_points(self):
+        """"""
         # TODO: animation of quick sort, means adding points one after the other
 
         for idx in range(self.n_graham_subs):
-            hull_color = self.colors[idx]
-            points_color = self.colors[idx]
+            hull_color = self.graham_colors[idx]
+            points_color = self.graham_colors[idx]
             self.ax.scatter(self.all_points[idx]['x'], self.all_points[idx]['y'], s=10, c=points_color, alpha=0.8, edgecolors='none')
             self.ax.scatter(self.final_sub_hulls[idx]['x'], self.final_sub_hulls[idx]['y'], s=10, c=hull_color, alpha=0.8, edgecolors='none')
 
@@ -135,7 +143,7 @@ class GrahamAnimation:
     def init_animation(self):
 
         for idx in range(self.n_graham_subs):
-            hull_color = self.colors[idx]
+            hull_color = self.graham_colors[idx]
             self.two_lines[idx], = self.ax.plot([], [], c=hull_color, alpha=0.8, linewidth=2)  # switching red / cyan
             self.hull_lines[idx], = self.ax.plot([], [], c=hull_color, alpha=0.8, linewidth=2)
         self.merge_lines, = self.ax.plot([], [], c=self.merge_color, alpha=0.9, linewidth=3)
