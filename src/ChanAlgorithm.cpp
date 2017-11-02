c75c916a4e787c2cf412d8e0646065397a6bcf33
Georg Kilzer <gkilzer@student.ethz.ch>
2017-11-02 23:19:59 +0100

    Remove now unused variable.

--- src/ChanAlgorithm.cpp
+++ src/ChanAlgorithm.cpp
@@ -184,8 +184,6 @@ std::vector<Point> ChanAlgorithm::run(const std::vector<Point>& points, size_t p
     std::vector<std::vector<Point> > hulls;
     hulls.resize(parallel_idx);
 
-    int currentSubsetIdx = 0;
-
     #pragma omp parallel for
     for (size_t i = 0; i < parallel_idx; ++i) {
         std::vector<Point> part = SplitVector(points, i, parallel_idx);
