bde37264ef5c20c0ee3029851fb63f61b1912c0c
Matthäus Heer <mathee@physik.uzh.ch>
2017-11-21 13:24:10 +0100

    Added param section

--- Visualization/AnimationWrapper.py
+++ Visualization/AnimationWrapper.py
@@ -11,12 +11,15 @@ from DataLoader import DataLoader
 import matplotlib.animation as animation
 
 
-# ----- SPECIFY PARAMETERS
+# ----- SPECIFY PARAMETERS ----- #
 
-save_movie = True
-show_labels = False
+save_movie      = False
+show_labels     = False
+interval        = 10  # controls the play-back speed
+output_name     = 'animation.mp4'
+original_image  = 'bird.jpg'  # set to None to not display any image
 
-# -----
+# ------------------------------ #
 
 input_data_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + '/Output'
 output_data_path = input_data_path
@@ -30,16 +33,16 @@ fig = plt.figure(figsize=(15, 9))
 animations = []
 
 ax = fig.add_subplot(111)
-graham_visualizer = ChanAnimation(data_dict, label_points=show_labels)
+graham_visualizer = ChanAnimation(data_dict, label_points=show_labels, original_image=original_image)
 graham_visualizer.set_ax(ax)
 graham_visualizer.plot_all_points()
 n_steps = max(graham_visualizer.n_steps_graham.values()) + graham_visualizer.n_steps_merge + 1
-
+print("Total steps will be: ", n_steps)
 
 animations.append(animation.FuncAnimation(fig,
                                           graham_visualizer.animate,
                                           frames=n_steps,
-                                          interval=10,
+                                          interval=interval,
                                           blit=False,
                                           init_func=graham_visualizer.init_animation,
                                           repeat=False))
@@ -49,6 +52,6 @@ Writer = animation.writers['ffmpeg']
 writer = Writer(fps=24, metadata=dict(artist='Me'), bitrate=1800)
 
 if save_movie:
-    animations[-1].save(output_data_path + '/graham_animation.mp4', writer=writer)
+    animations[-1].save(output_data_path + '/' + output_name, writer=writer)
 else:
     plt.show()
