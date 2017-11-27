2b412d3bc116fb0fd9dd55437f6b117b5a4c1504
Matthäus Heer <mathee@physik.uzh.ch>
2017-11-28 00:59:07 +0100

    Outsourced to python handler.

--- src/PointsGenerator/ShapeGenerator.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Polly Lilly on 19.11.17.
-//
-
-#include "../Point.h"
-std::vector<Point> randomShapeGenerator(int count, double range);
-std::vector<Point> circleShapeGenerator(int count, double range);
-std::vector<Point> squareShapeGenerator(int count, double range);
