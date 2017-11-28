48c0fc257edc50b12df7bb086b4ff19bb4312e25
Polly <polena.lilyanova@gmail.com>
2017-11-28 11:54:26 +0100

    Jarvis

--- src/JarvisAlgorithm.h
+++ src/JarvisAlgorithm.h
@@ -1,17 +1,13 @@
-//
-// Created by Polly Lilly on 23.11.17.
-//
-
-#pragma once
+#ifndef DPHPC_PROJECT_JARVISALGORITHM_H
+#define DPHPC_PROJECT_JARVISALGORITHM_H
 
 #include "utility.h"
 #include <algorithm>
 
 class JarvisAlgorithm {
-private:
-    int findTangentIndex(const std::vector<Point>& points, Point base);
-    int findNextPoint(const std::vector<Point>& points, Point base, int base_idx);
 public:
-    std::vector<Point> run(const std::vector<Point>& points, size_t parallel_idx);
+    std::vector<Point> run(const std::vector<Point>& points);
 };
 
+
+#endif //DPHPC_PROJECT_JARVISALGORITHM_H
